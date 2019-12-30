69a4c68de2ae8699304f1883dd3b944acec64d83
佐竹俊哉 <satashun@satashuns-iMac.local>
2019-12-31 03:13:13 +0900

    added modint

---
+++ src/math/modint.cpp
@@ -0,0 +1,55 @@
+template <unsigned int MOD>
+struct ModInt {
+	using ll = long long;
+	using uint = unsigned int;
+	using ull = unsigned long long;
+	using M = ModInt;
+
+	uint v;
+
+	ModInt(ll _v = 0) { set_norm(_v % MOD + MOD); }
+	M& set_norm(uint _v) { //[0, MOD * 2)->[0, MOD)
+		v = (_v < MOD) ? _v : _v - MOD;
+		return *this;
+	}
+
+	explicit operator bool() const { return v != 0; }
+	M operator+(const M& a) const { return M().set_norm(v + a.v); }
+	M operator-(const M& a) const { return M().set_norm(v + MOD - a.v); }
+	M operator*(const M& a) const { return M().set_norm(ull(v) * a.v % MOD); }
+	M operator/(const M& a) const { return *this * a.inv(); }
+	M& operator+=(const M& a) const { return *this = *this + a; }
+	M& operator-=(const M& a) const { return *this = *this - a; }
+	M& operator*=(const M& a) const { return *this = *this * a; }
+	M& operator/=(const M& a) const { return *this = *this / a; }
+	M operator-() const { return M() - *this; }
+	M& operator++(int) { return *this = *this + 1; }
+	M& operator--(int) { return *this = *this - 1; }
+
+	M pow(ll n) const {
+		if (n < 0) return inv().pow(-n);
+		M x = *this, res = 1;
+		while (n) {
+			if (n & 1) res *= x;
+			x *= x;
+			n >>= 1;
+		}
+		return res;
+	}
+
+	M inv() const {
+		ll a = v, b = MOD, p = 1, q = 0, t;
+		while (b != 0) {
+			t = a / b;
+			swap(a -= t * b, b);
+			swap(p -= t * q, q);
+		}
+		return M(p);
+	}
+
+	bool operator==(const M& a) const { return v == a.v; }
+	bool operator!=(const M& a) const { return v != a.v; }
+	friend ostream& operator<<(ostream& os, const M& a) { return os << a.v; }
+};
+
+using Mint = ModInt<1000000007>;
