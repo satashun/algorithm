1a446c4b1fdbf1362078c3d66d2b7886a06b9b50
佐竹俊哉 <satashun@satashuns-iMac.local>
2020-03-27 21:50:09 +0900

    to verify LCA

---
+++ test/yosupo/lca.test.cpp
@@ -0,0 +1,68 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/lca"
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define call_from_test
+#include "../../cpp_src/graph/LCA.hpp"
+#undef call_from_test
+
+typedef pair<int, int> pii;
+typedef long long ll;
+template<class T> using V = vector<T>;
+template<class T> using VV = V<V<T>>;
+
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define fi first
+#define se second
+#define rep(i,n) rep2(i,0,n)
+#define rep2(i,m,n) for(int i=m;i<(n);i++)
+#define ALL(c) (c).begin(),(c).end()
+
+#ifdef LOCAL
+#define dump(x) cerr << __LINE__ << " " << #x << " = " << (x) << endl
+#else 
+#define dump(x) true
+#endif
+
+constexpr ll TEN(int n) { return (n == 0) ? 1 : 10 * TEN(n-1); }
+
+template<class T, class U> void chmin(T& t, const U& u) { if (t > u) t = u; }
+template<class T, class U> void chmax(T& t, const U& u) { if (t < u) t = u; }
+
+template<class T, class U>
+ostream& operator<<(ostream& os, const pair<T, U>& p) {
+	os<<"("<<p.first<<","<<p.second<<")";
+	return os;
+}
+
+template<class T>
+ostream& operator<<(ostream& os, const vector<T>& v) {
+	os<<"{";
+	rep(i, v.size()) {
+		if (i) os<<",";
+		os<<v[i];
+	}
+	os<<"}";
+	return os;
+}
+
+int main() {
+	int N, Q; scanf("%d %d", &N, &Q);
+	V<int> p(N);
+	VV<int> g(N);
+	for (int i = 1; i < N; ++i) {
+		scanf("%d", &p[i]);
+		g[p[i]].pb(i);
+	}
+	LCA<int> lca(g, 0);
+	while (Q--) {
+		int a, b;
+		scanf("%d %d", &a, &b);
+		int v = lca.query(a, b);
+		printf("%d\n", v);
+	}
+	return 0;
+}
