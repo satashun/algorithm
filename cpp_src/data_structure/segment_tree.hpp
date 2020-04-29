cda155ba8ba8232ee1a4419d67c368ef29903ff7
Shunya Satake <satashun@satashuns-iMac.local>
2020-04-30 02:46:51 +0900

    segment tree (lazy propagation) added

--- cpp_src/data_structure/segment_tree.hpp
+++ cpp_src/data_structure/segment_tree.hpp
@@ -1,63 +1,65 @@
-//index of root = 1
+// index of root = 1
 
-template<class U>
+template <class U>
 struct segtree {
-	using T = typename U::T;
-	int sz;
-	V<T> dat;
+    using T = typename U::T;
+    int sz;
+    V<T> dat;
 
-	segtree(const V<T>& a) {
-		int n = a.size();
-		sz = 1;
-		while (sz < n) sz <<= 1;
-		dat.assign(sz * 2, U::id());
-		for (int i = 0; i < n; ++i) {
-			dat[sz + i] = a[i];
-		}
-		for (int i = sz - 1; i >= 1; --i) {
-			upd(i);
-		}
-	}
+    segtree() {}
+    segtree(int n) {
+        sz = 1;
+        while (sz < n) sz <<= 1;
+        dat.assign(sz * 2, U::id());
+    }
 
-	void upd(int p) {
-		dat[p] = U::op(dat[p<<1], dat[p<<1|1]);
-	}
+    segtree(const V<T>& a) {
+        int n = a.size();
+        sz = 1;
+        while (sz < n) sz <<= 1;
+        dat.assign(sz * 2, U::id());
+        for (int i = 0; i < n; ++i) {
+            dat[sz + i] = a[i];
+        }
+        for (int i = sz - 1; i >= 1; --i) {
+            upd(i);
+        }
+    }
 
-	void build() {
-		for (int i = sz - 1; i > 0; --i) {
-			dat[i] = U::op(dat[i<<1], dat[i<<1|1]);		
-		}
-	}
+    void upd(int p) { dat[p] = U::op(dat[p << 1], dat[p << 1 | 1]); }
 
-	void modify(int p, T v) {
-		p += sz;
-		dat[p] = v;
-		while (p >>= 1) {
-			dat[p] = U::op(dat[p<<1], dat[p<<1|1]);
-		}
-	}
+    void build() {
+        for (int i = sz - 1; i > 0; --i) {
+            dat[i] = U::op(dat[i << 1], dat[i << 1 | 1]);
+        }
+    }
 
-	//[l, r)
+    void modify(int p, T v) {
+        p += sz;
+        dat[p] = v;
+        while (p >>= 1) {
+            dat[p] = U::op(dat[p << 1], dat[p << 1 | 1]);
+        }
+    }
 
-	T query(int l, int r) {
-		T lval = U::id(), rval = U::id();
-		int res = 0;
-		for (l += sz, r += sz; l < r; l >>= 1, r >>= 1) {
-			if (l & 1) lval = U::op(lval, dat[l++]);
-			if (r & 1) rval = U::op(dat[--r], rval);
-		}
-		return U::op(lval, rval);
-	}
+    //[l, r)
+
+    T query(int l, int r) {
+        T lval = U::id(), rval = U::id();
+        for (l += sz, r += sz; l < r; l >>= 1, r >>= 1) {
+            if (l & 1) lval = U::op(lval, dat[l++]);
+            if (r & 1) rval = U::op(dat[--r], rval);
+        }
+        return U::op(lval, rval);
+    }
 };
 
-//modify only U to use
+// modify only U for use
 
-const int INF = TEN(9) + 10;
+constexpr ll INF = TEN(9) + 10;
 
 struct U {
     using T = ll;
     static T id() { return INF; }
-    static T op(const T& a, const T& b) {
-    	return min(a, b);
-    }
+    static T op(const T& a, const T& b) { return min(a, b); }
 };
