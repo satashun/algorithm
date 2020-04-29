a7ec03673f004ab5e53370b706f9b59632d41d58
佐竹俊哉 <satashun@satashuns-iMac.local>
2020-04-30 02:45:53 +0900

    segment tree (lazy propagation) added

--- cpp_src/math/matrix.hpp
+++ cpp_src/math/matrix.hpp
@@ -1,39 +1,39 @@
-template<class T>
+template <class T>
 struct Matrix : VV<D> {
-	using Mat = Matrix<T>;
-	int H() const { return int(size()); }
-	int W() const { return int((*this)[0].size(); )}
+    using Mat = Matrix<T>;
+    int H() const { return int(size()); }
+    int W() const { return int((*this)[0].size();) }
 
-	Mat operator*(const Mat& a) const {
-		int h = H(), w = W();
-		int h2 = a.H(), w2 = a.W();
-		assert(w == h2);
-		Mat res(h, V<T>(w2));
+    Mat operator*(const Mat& a) const {
+        int h = H(), w = W();
+        int h2 = a.H(), w2 = a.W();
+        assert(w == h2);
+        Mat res(h, V<T>(w2));
 
-		for (int i = 0; i < h; ++i) {
-			for (int j = 0; j < w2; ++j) {
-				for (int k = 0; k < w; ++k) {
-					res[i][j] += (*this)[i][k] * a[k][j];
-				}
-			}
-		}
-		return res;
-	}
+        for (int i = 0; i < h; ++i) {
+            for (int j = 0; j < w2; ++j) {
+                for (int k = 0; k < w; ++k) {
+                    res[i][j] += (*this)[i][k] * a[k][j];
+                }
+            }
+        }
+        return res;
+    }
 
-	Mat& operator*=(const Mat& r) { return *this = *this * r; }
+    Mat& operator*=(const Mat& r) { return *this = *this * r; }
 
-	Mat pow(ll n) const {
-		assert(H() == W());
-		int h = H();
-		Mat x = *this, res(h, V<T>(h));
-		for (int i = 0; i < h; ++i) {
-			res[i] = T(1);
-		}
-		while (n > 0) {
-			if (n & 1) res *= x;
-			x *= x;
-			n >>= 1;
-		}
-		return res;
-	}
+    Mat pow(ll n) const {
+        assert(H() == W());
+        int h = H();
+        Mat x = *this, res(h, V<T>(h));
+        for (int i = 0; i < h; ++i) {
+            res[i] = T(1);
+        }
+        while (n > 0) {
+            if (n & 1) res *= x;
+            x *= x;
+            n >>= 1;
+        }
+        return res;
+    }
 };
