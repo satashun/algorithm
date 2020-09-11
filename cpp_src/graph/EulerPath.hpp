cf8da237dd071fa2809bfffe9dbc726c2870bc4b
佐竹俊哉 <satashun@satashuns-MacBook-Pro.local>
2020-09-11 21:28:31 +0900

    zaki

---
+++ cpp_src/graph/EulerPath.hpp
@@ -0,0 +1,21 @@
+// Hierholzer algorithm
+// https://codeforces.com/contest/508/submission/92099335
+
+// directed
+// destruct graph
+
+template <class E>
+V<int> EulerianPath(const int s, VV<E>& g) {
+    auto dfs = [&](auto&& self, int u, V<int>& path) -> void {
+        while (!g[u].empty()) {
+            int v = g[u].back();  // E : int
+            g[u].pop_back();
+            self(self, v, path);
+        }
+        path.push_back(u);
+    };
+    V<int> path;
+    dfs(dfs, s, path);
+    reverse(path.begin(), path.end());
+    return path;
+}
