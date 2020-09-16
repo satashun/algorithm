36e5f8e139b0c5bc636758383da511957cb49b9c
Shunya Satake <satashun@users.noreply.github.com>
2020-09-17 02:07:27 +0900

    Revert "add prod of polynomials, div_floor"
    This reverts commit e08347c67581421559a71788b1681dabbd11f47b.

--- cpp_src/math/useful.hpp
+++ cpp_src/math/useful.hpp
@@ -1,28 +1,18 @@
-// floor(a / b)
-ll div_floor(ll a, ll b) {
-    return a / b - (((a ^ b) < 0 && a % b != 0) ? 1 : 0);
-}
-
-// ceil(a / b)
-ll div_ceil(ll a, ll b) {
-    return a / b + (((a ^ b) > 0 && a % b != 0) ? 1 : 0);
-}
-
 constexpr ll floor_sqrt(ll n) {
-    if (n == 0) return 0;
-    return llround(floor(sqrt(n)));
+	if (n == 0) return 0;
+	return llround(floor(sqrt(n)));
 }
 
-// from USACO
+//from USACO
 using ull = unsigned long long;
 using L = __uint128_t;
 
 struct FastMod {
-    ull b, m;
-    FastMod(ull b) : b(b), m(ull((L(1) << 64) / b)) {}
-    ull reduce(ull a) {
-        ull q = (ull)((L(m) * a) >> 64);
-        ull r = a - q * b;  // can be proven that 0 <= r < 2*b
-        return r >= b ? r - b : r;
-    }
+	ull b, m;
+	FastMod(ull b) : b(b), m(ull((L(1) << 64) / b)) {}
+	ull reduce(ull a) {
+		ull q = (ull)((L(m) * a) >> 64);
+		ull r = a - q * b; // can be proven that 0 <= r < 2*b
+		return r >= b ? r - b : r;
+	}
 };
