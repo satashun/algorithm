8734a98b529adcbc465afcbd0eb2f32fe7742db9
Shunya Satake <satashun@users.noreply.github.com>
2020-09-17 22:27:47 +0900

    Capitalize

---
+++ cpp_src/utility/Random.hpp
@@ -0,0 +1,18 @@
+ll rand_int(ll l, ll r) {  //[l,r]
+    static random_device rd;
+    static mt19937 gen(rd());
+    // static mt19937 gen(chrono::steady_clock::now().time_since_epoch().count());
+    return uniform_int_distribution<ll>(l, r)(gen);
+}
+
+// Fisher–Yates
+template <class T>
+vector<T> random_perm(vector<T> vec) {
+    vector<T> res;
+    rep(i, vec.size()) {
+        res.pb(vec[i]);
+        int j = rand_int(0, i);
+        swap(res[j], res[i]);
+    }
+    return res;
+}
