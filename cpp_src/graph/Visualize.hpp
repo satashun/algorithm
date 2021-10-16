c1a8d8bb31f4ed8da1b1edade513b41bdcf6e77f
Shunya Satake <satashun@users.noreply.github.com>
2021-10-17 02:35:14 +0900

    fixed graphbase

--- cpp_src/graph/Visualize.hpp
+++ cpp_src/graph/Visualize.hpp
@@ -1,5 +1,5 @@
 // label : index
-// dot -Tsvg a.dot -o a.svg
+// dot -Tsvg a.DOT -o a.svg
 
 template <class T>
 void to_graphviz(const Graph<T> g, string filename, bool directed = false) {
@@ -8,7 +8,8 @@ void to_graphviz(const Graph<T> g, string filename, bool directed = false) {
         ss << "digraph{\n";
         for (int i = 0; i < g.size(); ++i) {
             for (auto e : g[i]) {
-                ss << i << "->" << e.to << "[label=" << e.idx << "];\n";
+                ss << i << "->" << e.to << "[label=\"index=" << e.idx
+                   << ", cost=" << e.cost << "\"];\n";
             }
         }
     } else {
@@ -16,7 +17,8 @@ void to_graphviz(const Graph<T> g, string filename, bool directed = false) {
         for (int i = 0; i < g.size(); ++i) {
             for (auto e : g[i]) {
                 if (e.to > i) {
-                    ss << i << "--" << e.to << "[label=" << e.idx << "];\n";
+                    ss << i << "--" << e.to << "[label=\"index=" << e.idx
+                       << ", cost=" << e.cost << "\"];\n";
                 }
             }
         }
