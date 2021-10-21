3840a058a01620a796384cc4d46767659195e245
Shunya Satake <satashun@users.noreply.github.com>
2021-10-21 19:43:39 +0900

    added Lagrange

--- cpp_src/graph/TreeDP.hpp
+++ cpp_src/graph/TreeDP.hpp
@@ -1,19 +1,24 @@
+// ABC160F, ABC223G
+
 template <class N, class E>
 class treedp {
+    // a: temporary, b[v] : left of v (when rooted at some vertex)
+    // after dfs_pre, a[v] means(dp on subtree at v) and consider the edge to the parent
+    
    public:
     int n;
-    const VV<E>& g;
-    V<N> a, b;  // temporary, b : left
+    const Graph<E>& g;
+    V<N> a, b;
     V<N> res, sub, bus;
 
     void dfs_pre(int v, int p) {
         a[v].init(v);
         E up;
         for (auto e : g[v]) {
-            if (e != p) {
-                dfs_pre(e, v);
-                b[e] = a[v];
-                a[v] = a[v] + a[e];
+            if (e.to != p) {
+                dfs_pre(e.to, v);
+                b[e.to] = a[v];
+                a[v] = a[v] + a[e.to];
             } else {
                 up = e;
             }
@@ -25,43 +30,44 @@ class treedp {
     void dfs_all(int v, int p, N cur) {
         for (int i = int(g[v].size()) - 1; i >= 0; --i) {
             auto e = g[v][i];
-            if (e == p) continue;
-            bus[e] = cur + b[e];
-            dfs_all(e, v, bus[e].up(e));
-            cur = cur + a[e];
+            if (e.to == p) continue;
+            bus[e.to] = cur + b[e.to];
+            dfs_all(e.to, v, bus[e.to].up(e));
+            cur = cur + a[e.to];
         }
         res[v] = cur;
     }
 
-    treedp(const VV<E>& gg, int root = 0)
+    treedp(const Graph<E>& gg, int root = 0)
         : n(gg.size()), g(gg), a(n), b(n), res(n), sub(n), bus(n) {
         dfs_pre(root, -1);
-        N tmp;
-        tmp.init(root);
+        N tmp;  // identity
         dfs_all(root, -1, tmp);
+    }
+
+    void slv() {
     }
 };
 
 /*
-struct N {
+// template<class E>
+struct Data {
+    Data() {}
     void init(int v) {}
 
     // v -> to -> *
     // after calculation on vertices under to
     // consider the edge (v - to)
 
-    N up(int to) const {
-        N res = *this;
+    // Data up(E to) const {
+    Data up(int to) const {
+        Data res = *this;
         return res;
     }
 
-    N operator+(const N& r) const {
-        N res = *this;
+    Data operator+(const Data& r) const {
+        Data res = *this;
         return res;
     }
 };
-
-void slv() {
-    int n;
-    treedp<N, int> dp(n);
-}*/
+*/
---
+++ cpp_src/math/Lagrange.cpp
@@ -0,0 +1,21 @@
+// https://github.com/ei1333/library/blob/master/math/combinatorics/lagrange-polynomial.cpp
+// y_i = f(i) for i = 0, 1, ..., N
+// evaluate f(t)
+
+template <typename T>
+T lagrange_polynomial(const vector<T> &y, ll t) {
+    int N = y.size() - 1;
+    if (t <= N) return y[t];
+    T ret(0);
+    vector<T> dp(N + 1, 1), pd(N + 1, 1);
+    for (int i = 0; i < N; i++) dp[i + 1] = dp[i] * (t - i);
+    for (int i = N; i > 0; i--) pd[i - 1] = pd[i] * (t - i);
+    for (int i = 0; i <= N; i++) {
+        T tmp = y[i] * dp[i] * pd[i] * ifact[i] * ifact[N - i];
+        if ((N - i) & 1)
+            ret -= tmp;
+        else
+            ret += tmp;
+    }
+    return ret;
+}
