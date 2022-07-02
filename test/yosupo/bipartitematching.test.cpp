76d1c6157b0de4bc337e14f505e337ad87c6aee6
Shunya Satake <satashun@users.noreply.github.com>
2022-07-02 20:38:49 +0900

    waf

--- test/yosupo/bipartitematching.test.cpp
+++ test/yosupo/bipartitematching.test.cpp
@@ -1,13 +1,28 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/bipartitematching"
+#pragma region satashun
+//#pragma GCC optimize("Ofast")
+//#pragma GCC optimize("unroll-loops")
 #include <bits/stdc++.h>
 using namespace std;
 
+using uint = unsigned int;
 using ll = long long;
+using ull = unsigned long long;
 using pii = pair<int, int>;
 template <class T>
 using V = vector<T>;
 template <class T>
 using VV = V<V<T>>;
+
+template <class T>
+V<T> make_vec(size_t a) {
+    return V<T>(a);
+}
+
+template <class T, class... Ts>
+auto make_vec(size_t a, Ts... ts) {
+    return V<decltype(make_vec<T>(ts...))>(a, make_vec<T>(ts...));
+}
 
 #define pb push_back
 #define eb emplace_back
@@ -19,6 +34,7 @@ using VV = V<V<T>>;
 #define per(i, b) per2(i, 0, b)
 #define per2(i, a, b) for (int i = int(b) - 1; i >= int(a); i--)
 #define ALL(c) (c).begin(), (c).end()
+#define SZ(x) ((int)(x).size())
 
 constexpr ll TEN(int n) { return (n == 0) ? 1 : 10 * TEN(n - 1); }
 
@@ -31,10 +47,45 @@ void chmax(T& t, const U& u) {
     if (t < u) t = u;
 }
 
+template <class T>
+void mkuni(vector<T>& v) {
+    sort(ALL(v));
+    v.erase(unique(ALL(v)), end(v));
+}
+
+template <class T>
+vector<int> sort_by(const vector<T>& v, bool increasing = true) {
+    vector<int> res(v.size());
+    iota(res.begin(), res.end(), 0);
+
+    if (increasing) {
+        stable_sort(res.begin(), res.end(),
+                    [&](int i, int j) { return v[i] < v[j]; });
+    } else {
+        stable_sort(res.begin(), res.end(),
+                    [&](int i, int j) { return v[i] > v[j]; });
+    }
+    return res;
+}
+
+template <class T, class U>
+istream& operator>>(istream& is, pair<T, U>& p) {
+    is >> p.first >> p.second;
+    return is;
+}
+
 template <class T, class U>
 ostream& operator<<(ostream& os, const pair<T, U>& p) {
     os << "(" << p.first << "," << p.second << ")";
     return os;
+}
+
+template <class T>
+istream& operator>>(istream& is, vector<T>& v) {
+    for (auto& x : v) {
+        is >> x;
+    }
+    return is;
 }
 
 template <class T>
@@ -63,19 +114,64 @@ void debug_out(Head H, Tail... T) {
 #define dump(x) (void(0))
 #endif
 
+template <class T>
+void scan(vector<T>& v, T offset = T(0)) {
+    for (auto& x : v) {
+        cin >> x;
+        x += offset;
+    }
+}
+
+// suc : 1 = newline, 2 = space
+template <class T>
+void print(T x, int suc = 1) {
+    cout << x;
+    if (suc == 1)
+        cout << "\n";
+    else if (suc == 2)
+        cout << " ";
+}
+
+template <class T>
+void print(const vector<T>& v, int suc = 1) {
+    for (int i = 0; i < v.size(); ++i)
+        print(v[i], i == int(v.size()) - 1 ? suc : 2);
+}
+
+template <class T>
+void show(T x) {
+    print(x, 1);
+}
+
+template <typename Head, typename... Tail>
+void show(Head H, Tail... T) {
+    print(H, 2);
+    show(T...);
+}
+
+struct prepare_io {
+    prepare_io() {
+        cin.tie(nullptr);
+        ios::sync_with_stdio(false);
+        cout << fixed << setprecision(10);
+    }
+} prep_io;
+#pragma endregion satashun
+
 #define call_from_test
 #include "../../cpp_src/graph/Dinic.hpp"
 #undef call_from_test
 
 int main() {
     int L, R, M;
-    scanf("%d %d %d", &L, &R, &M);
+    cin >> L >> R >> M;
     int n = L + R + 2;
     int s = n - 2, t = n - 1;
     Dinic<ll> g(n);
+
     rep(i, M) {
         int a, b;
-        scanf("%d%d", &a, &b);
+        cin >> a >> b;
         g.add_edge(a, b + L, 1);
     }
     rep(i, L) g.add_edge(s, i, 1);
@@ -83,7 +179,8 @@ int main() {
 
     int K = g.max_flow(s, t);
 
-    printf("%d\n", K);
+    show(K);
+
     rep(i, L) {
         int mt = -1;
         for (auto e : g.g[i])
@@ -91,7 +188,7 @@ int main() {
                 mt = e.to;
             }
         if (mt != -1) {
-            printf("%d %d\n", i, mt - L);
+            show(i, mt - L);
         }
     }
 
